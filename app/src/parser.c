3985d0cd0026441f926028720bd9b675f5844ef4
Leon Groot Bruinderink <lgrootbr@gmail.com>
2021-05-06 15:55:22 +0200

    support for more types in UI

--- app/src/parser.c
+++ app/src/parser.c
@@ -104,7 +104,7 @@ parser_error_t parser_getNumItems(const parser_context_t *ctx, uint8_t *num_item
     uint8_t value = 0;                                                     \
     readU8(ctx, &value);                                                   \
     if (value == 0x00) {                                             \
-        snprintf(outVal, outValLen, "True");                \
+        snprintf(outVal, outValLen, "True");                        \
     }else{                                                           \
         snprintf(outVal, outValLen, "False");                      \
     }                                                               \
@@ -168,6 +168,26 @@ parser_error_t parser_getNumItems(const parser_context_t *ctx, uint8_t *num_item
     return parser_ok;                                                                     \
 }
 
+#define DISPLAY_RUNTIMEARG_KEY(CTX, TYPE, TYPELEN, LEN){     \
+    char buffer[300];                                       \
+    MEMZERO(buffer, sizeof(buffer));                         \
+    MEMCPY(buffer, (TYPE), TYPELEN);                                                         \
+    array_to_hexstr(buffer + (TYPELEN), sizeof(buffer)- (TYPELEN), (CTX)->buffer + (CTX)->offset + 1, LEN); \
+    pageString(outVal, outValLen, (char *) buffer, pageIdx, pageCount); \
+    return parser_ok;                              \
+}
+
+#define DISPLAY_RUNTIMEARG_UREF(CTX, LEN){     \
+    char buffer[300];                                       \
+    MEMZERO(buffer, sizeof(buffer));                         \
+    MEMCPY(buffer, (char *)"uref-", 5);                                                         \
+    array_to_hexstr(buffer + 5, sizeof(buffer)-5, (CTX)->buffer + (CTX)->offset, (LEN)-1);      \
+    MEMCPY(buffer + 4 + (LEN), (char *)"-0", 2); \
+    MEMCPY(buffer + 6 + (LEN), (CTX)->buffer + (CTX)->offset + (LEN), 1);       \
+    pageString(outVal, outValLen, (char *) buffer, pageIdx, pageCount); \
+    return parser_ok;                              \
+}
+
 parser_error_t parser_getItem_RuntimeArgs(parser_context_t *ctx,
                                           uint8_t displayIdx,
                                           char *outKey, uint16_t outKeyLen,
@@ -196,35 +216,90 @@ parser_error_t parser_getItem_RuntimeArgs(parser_context_t *ctx,
     CHECK_PARSER_ERR(readU32(ctx, &dataLen));
     uint8_t type = *(ctx->buffer + ctx->offset + dataLen);
     switch (type) {
-        case 0x00: {
+        case 0: {
             DISPLAY_RUNTIMEARG_BOOLEAN(ctx)
         }
-        case 0x01: {
+        case 1: {
             DISPLAY_RUNTIMEARG_I32(ctx)
         }
-        case 0x02: {
+        case 2: {
             DISPLAY_RUNTIMEARG_I64(ctx)
         }
-        case 0x03: {
+        case 3: {
             DISPLAY_RUNTIMEARG_U8(ctx)
         }
-        case 0x04: {
+        case 4: {
             DISPLAY_RUNTIMEARG_U32(ctx)
         }
-        case 0x05: {
+        case 5: {
             DISPLAY_RUNTIMEARG_U64(ctx)
         }
 
-        case 0x06: {
-            DISPLAY_RUNTIMEARG_BYTES(ctx, 16)
+        case 6: {
+            DISPLAY_RUNTIMEARG_BYTES(ctx, dataLen)
         }
 
-        case 0x07: {
-            DISPLAY_RUNTIMEARG_BYTES(ctx, 32)
+        case 7: {
+            DISPLAY_RUNTIMEARG_BYTES(ctx, dataLen)
         }
 
-        case 0x08: {
-            DISPLAY_RUNTIMEARG_BYTES(ctx, 64)
+        case 8: {
+            DISPLAY_RUNTIMEARG_BYTES(ctx, dataLen)
+        }
+/*
+ * const CL_TYPE_TAG_UNIT: u8 = 9;
+const CL_TYPE_TAG_STRING: u8 = 10;
+const CL_TYPE_TAG_KEY: u8 = 11;
+const CL_TYPE_TAG_UREF: u8 = 12;
+const CL_TYPE_TAG_OPTION: u8 = 13;
+ */
+        case 9: {
+            DISPLAY_RUNTIMEARG_BYTES(ctx, dataLen)
+        }
+
+        case 10 : {
+            DISPLAY_RUNTIMEARG_STRING(ctx, dataLen)
+        }
+
+        case 11 : {
+            uint8_t keytype = 0;
+            CHECK_PARSER_ERR(readU8(ctx, &keytype));
+            switch(keytype) {
+                case 0x00 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "account-hash-",13, dataLen -1);
+                }
+                case 0x01 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "hash-",5, dataLen - 1);
+                }
+                case 0x02 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "uref-",5, dataLen -1);
+                }
+                case 0x03 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "transfer-",9, dataLen -1);
+                }
+                case 0x04 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "deploy-",7, dataLen -1);
+                }
+                case 0x05 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "era-",4, dataLen -1);
+                }
+                case 0x06 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "balance-",8, dataLen -1);
+                }
+                case 0x07 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "bid-",4, dataLen -1);
+                }
+                case 0x08 : {
+                    DISPLAY_RUNTIMEARG_KEY(ctx, "withdraw-",9, dataLen -1);
+                }
+                default : {
+                    return parser_unexepected_error;
+                }
+            }
+        }
+
+        case 12 : {
+            DISPLAY_RUNTIMEARG_UREF(ctx, dataLen);
         }
 
         default : {
