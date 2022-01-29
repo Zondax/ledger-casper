91f7a8d1e51492035c7ea63d3211ddc07504196a
Fernando Theirs <fntheirs@gmail.com>
2022-01-29 19:10:21 +0100

    change time utils format

--- deps/ledger-zxlib/src/timeutils.c
+++ deps/ledger-zxlib/src/timeutils.c
@@ -493,10 +493,10 @@ zxerr_t printTime(char *out, uint16_t outLen, uint64_t t) {
     const char *monthName = getMonth(tm_mon);
 
     // YYYYmmdd HH:MM:SS
-    snprintf(out, outLen, "%02d%s%04d %02d:%02d:%02dUTC",
-             tm_day,
-             monthName,
+    snprintf(out, outLen, "%d-%02d-%02dT%02d:%02d:%02dZ",
              tm_year,
+             tm_mon,
+             tm_day,
              tm_hour, tm_min, tm_sec
     );
 
