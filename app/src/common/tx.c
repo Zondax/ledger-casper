f65bce02db1b5f6e204c1353381009b4a28ee729
0xPxt <raventosaleix@gmail.com>
2025-04-29 15:19:26 +0200

    Revert "Remove nanoS (#115)"
    This reverts commit 10afe1f2e4eeb8dd789da9187c056e99db2a441f.

--- app/src/common/tx.c
+++ app/src/common/tx.c
@@ -24,10 +24,13 @@
 #include "parser_txdef.h"
 #include "zxmacros.h"
 
-#if defined(LEDGER_SPECIFIC)
+#if defined(TARGET_NANOS)
+#define RAM_BUFFER_SIZE 384
+#define FLASH_BUFFER_SIZE 8192
+#else
 #define RAM_BUFFER_SIZE 8192
 #define FLASH_BUFFER_SIZE 16384
-
+#endif
 // Ram
 uint8_t ram_buffer[RAM_BUFFER_SIZE];
 
@@ -36,6 +39,7 @@ typedef struct {
     uint8_t buffer[FLASH_BUFFER_SIZE];
 } storage_t;
 
+#if defined(LEDGER_SPECIFIC)
 storage_t NV_CONST N_appdata_impl __attribute__((aligned(64)));
 #define N_appdata (*(NV_VOLATILE storage_t *)PIC(&N_appdata_impl))
 #endif
